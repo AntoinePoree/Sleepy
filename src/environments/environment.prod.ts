dd10fffe7edb50e538a24389187e0f527975826d
AntoinePoree <poree47@gmail.com>
2020-03-19 22:24:22 +0100

    1.2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,6 +11,6 @@ export const environment = {
   hmr: false,
   version: env.npm_package_version,
   serverUrl: 'https://api.chucknorris.io',
-  defaultLanguage: 'en-US',
+  defaultLanguage: 'fr-FR',
   supportedLanguages: ['en-US', 'fr-FR']
 };
